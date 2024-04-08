50f7b1a9d2776ba6975fabd2192c2bcdc18bd906
FireStreaker2 <michaelccc2019@outlook.com>
2024-04-07 21:08:47 -0700

    react terminal template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { TerminalContextProvider } from "react-terminal";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<TerminalContextProvider>
+			<Component {...pageProps} />
+		</TerminalContextProvider>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,25 @@
+import { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
-import { useState } from "react";
+import { ReactTerminal } from "react-terminal";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type Theme = "light" | "dark";
 interface Config {
 	host: string;
 	port: string;
 	username: string;
 	password: string;
-	command: string;
 }
 
 export default function Home() {
-	const [result, setResult] = useState("");
+	const [theme, setTheme] = useState<Theme>("light");
+	const [loggedIn, setLoggedIn] = useState(false);
 	const [config, setConfig] = useState<Config>({
 		host: "",
 		port: "",
 		username: "",
 		password: "",
-		command: "",
 	});
 
 	const handleInputChange = (key: keyof Config, value: string) => {
@@ -28,77 +29,83 @@ export default function Home() {
 		}));
 	};
 
-	const submit = async (event: { preventDefault: () => void }) => {
-		event.preventDefault();
-
-		if (config.host === "" || config.username === "" || config.command === "")
-			return alert("missing parameters!");
-
-		if (config.port === "") config.port = "22";
-
-		const response = await fetch("/api/connect", {
-			method: "POST",
-			body: JSON.stringify(config),
-		});
-
-		if (!response.ok) return alert("an error occurred when fetching!");
-
-		const data = await response.json();
-
-		setResult(data?.message);
-	};
+	useEffect(() => {
+		setTheme(
+			window.matchMedia &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches
+				? "dark"
+				: "light"
+		);
+	}, []);
 
 	return (
-		<main className={inter.className}>
-			<form>
-				<div className="flex flex-col">
-					<h1>Server</h1>
-					<input
-						placeholder="host"
-						className="text-black"
-						onChange={(event) => handleInputChange("host", event.target.value)}
-					/>
-					<input
-						placeholder="port"
-						className="text-black"
-						onChange={(event) => handleInputChange("port", event.target.value)}
-					/>
-				</div>
-				<div className="flex flex-col">
-					<h1>Credentials</h1>
-					<input
-						placeholder="username"
-						className="text-black"
-						onChange={(event) =>
-							handleInputChange("username", event.target.value)
-						}
-					/>
-					<input
-						placeholder="password"
-						type="password"
-						className="text-black"
-						onChange={(event) =>
-							handleInputChange("password", event.target.value)
-						}
-					/>
-				</div>
-				<div>
-					<h1>Command</h1>
-					<input
-						placeholder="command"
-						className="text-black"
-						onChange={(event) =>
-							handleInputChange("command", event.target.value)
-						}
-					/>
-				</div>
+		<main className={`w-full h-screen ${inter.className}`}>
+			{loggedIn ? (
+				<ReactTerminal
+					theme={theme}
+					prompt="$"
+					errorMessage={(command: string) =>
+						`webssh: ${command}: command not found`
+					}
+					// TODO: add command logic (fetch /api/connect endpoint)
+					defaultHandler={(command: string) => JSON.stringify(config)}
+				/>
+			) : (
+				<form>
+					<div className="flex flex-col">
+						<h1>Server</h1>
+						<input
+							placeholder="host"
+							className="text-black"
+							onChange={(event) =>
+								handleInputChange("host", event.target.value)
+							}
+						/>
+						<input
+							placeholder="port"
+							className="text-black"
+							onChange={(event) =>
+								handleInputChange("port", event.target.value)
+							}
+						/>
+					</div>
+					<div className="flex flex-col">
+						<h1>Credentials</h1>
+						<input
+							placeholder="username"
+							className="text-black"
+							onChange={(event) =>
+								handleInputChange("username", event.target.value)
+							}
+						/>
+						<input
+							placeholder="password"
+							type="password"
+							className="text-black"
+							onChange={(event) =>
+								handleInputChange("password", event.target.value)
+							}
+						/>
+					</div>
 
-				<button onClick={submit}>Login</button>
-			</form>
+					<button
+						onClick={(event) => {
+							event.preventDefault();
 
-			<div>
-				<p>{result}</p>
-			</div>
+							// TODO: use something thats not an alert
+							if (config.host === "" || config.username === "")
+								return alert("missing parameters!");
+
+							if (config.port === "") config.port = "22";
+
+							// TODO: check if you can actually ssh in before rendering terminal
+							setLoggedIn(true);
+						}}
+					>
+						Login
+					</button>
+				</form>
+			)}
 		</main>
 	);
 }
