0dae8da94791ed7eaa1fdb2e8776af05f5d2e21f
FireStreaker2 <michaelccc2019@outlook.com>
2024-04-10 11:17:04 -0700

    improved login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
 import { ReactTerminal } from "react-terminal";
+import { SpinningCircles } from "react-loading-icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ interface Data {
 }
 
 export default function Home() {
-	const [theme, setTheme] = useState<Theme>("light");
+	const [loading, setLoading] = useState(false);
 	const [loggedIn, setLoggedIn] = useState(false);
+	const [error, setError] = useState("");
+	const [theme, setTheme] = useState<Theme>("light");
 	const [config, setConfig] = useState<Config>({
 		host: "",
 		port: "",
@@ -28,6 +31,7 @@ export default function Home() {
 	});
 
 	const handleInputChange = (key: keyof Config, value: string) => {
+		setLoading(false);
 		setConfig((prevConfig) => ({
 			...prevConfig,
 			[key]: value,
@@ -48,7 +52,8 @@ export default function Home() {
 			{loggedIn ? (
 				<ReactTerminal
 					theme={theme}
-					prompt="$"
+					prompt={`[${config.username}@${config.host}]$`}
+					welcomeMessage={`Welcome to WebSSH! You are logged in as: ${config.username}`}
 					errorMessage={(command: string) =>
 						`webssh: ${command}: command not found`
 					}
@@ -69,71 +74,89 @@ export default function Home() {
 					}}
 				/>
 			) : (
-				<form>
-					<div className="flex flex-col">
-						<h1>Server</h1>
-						<input
-							placeholder="host"
-							className="text-black"
-							onChange={(event) =>
-								handleInputChange("host", event.target.value)
-							}
-						/>
-						<input
-							placeholder="port"
-							className="text-black"
-							onChange={(event) =>
-								handleInputChange("port", event.target.value)
-							}
-						/>
-					</div>
-					<div className="flex flex-col">
-						<h1>Credentials</h1>
-						<input
-							placeholder="username"
-							className="text-black"
-							onChange={(event) =>
-								handleInputChange("username", event.target.value)
-							}
-						/>
-						<input
-							placeholder="password"
-							type="password"
-							className="text-black"
-							onChange={(event) =>
-								handleInputChange("password", event.target.value)
-							}
-						/>
-					</div>
+				<div className="w-full h-full flex justify-center items-center">
+					<form className="border dark:border-white border-black flex justify-center items-center text-center flex-col gap-4 w-96 h-96">
+						<h1 className="text-3xl">WebSSH</h1>
+						<div className="flex flex-col">
+							<h1 className="text-xl">Server</h1>
+							<input
+								placeholder="Host"
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								onChange={(event) =>
+									handleInputChange("host", event.target.value)
+								}
+							/>
+							<input
+								placeholder="Port"
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								onChange={(event) =>
+									handleInputChange("port", event.target.value)
+								}
+							/>
+						</div>
+						<div className="flex flex-col">
+							<h1 className="text-xl">Credentials</h1>
+							<input
+								placeholder="Username"
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								onChange={(event) =>
+									handleInputChange("username", event.target.value)
+								}
+							/>
+							<input
+								placeholder="Password"
+								type="password"
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								onChange={(event) =>
+									handleInputChange("password", event.target.value)
+								}
+							/>
+						</div>
 
-					<button
-						onClick={async (event) => {
-							event.preventDefault();
+						<div className="relative w-full h-5 flex justify-center items-center">
+							{error && <p className="absolute text-red-500">{error}</p>}
+							{loading && <SpinningCircles />}
+						</div>
 
-							// TODO: use something thats not an alert
-							if (config.host === "" || config.username === "")
-								return alert("missing parameters!");
+						<button
+							className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+							onClick={async (event) => {
+								event.preventDefault();
 
-							if (config.port === "") config.port = "22";
+								if (config.host === "" || config.username === "")
+									return setError("missing parameters!");
 
-							const response = await fetch("/api/connect", {
-								method: "POST",
-								body: JSON.stringify({
-									...config,
-									command: "ls",
-								}),
-							});
+								if (config.port === "") {
+									config.port = "22";
+									console.log("defaulting port to 22");
+								}
 
-							if (!response.ok) return alert("an error occurred");
+								setLoading(true);
 
-							const data: Data = await response.json();
+								const response = await fetch("/api/connect", {
+									method: "POST",
+									body: JSON.stringify({
+										...config,
+										command: "ls",
+									}),
+								});
 
-							data.error ? alert("error while logging in") : setLoggedIn(true);
-						}}
-					>
-						Login
-					</button>
-				</form>
+								if (!response.ok) return setError("an http error occurred!");
+
+								const data: Data = await response.json();
+
+								if (data.error)
+									setError(`an error occurred while logging in: ${data.error}`);
+								else {
+									setLoading(false);
+									setLoggedIn(true);
+								}
+							}}
+						>
+							Login
+						</button>
+					</form>
+				</div>
 			)}
 		</main>
 	);
